9835327ca35f0136f3e8aa6cfbab0a2f33050207
NicholasManzo <NicholasManzo@WBI-2160>
2022-10-24 09:44:10 -0400

    Add project files.

---
+++ SourceManagement/Source.cpp
@@ -0,0 +1,33 @@
+/*
+Source Code Management
+Nick Manzo
+10/18/22
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void Hello_World();
+
+int main() {
+
+	
+	Hello_World();
+
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
+
+
+void Hello_World() {
+	cout << "\nHello World! Im on GitHub!!!";
+
+	for (int i = 0; i < 3; i++) {
+		cout << "\n.";
+	}
+	cout << "\nGood Bye World, Nice knowing you!";
+}
+
